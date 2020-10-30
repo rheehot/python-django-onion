db87b64d10e71cb70c821ded03d65eac4c86152e
suhyunbaik <suhyunbaik89@gmail.com>
2020-10-30 14:49:34 +0900

    :recycle: change usecase to application service

--- cafe/application_service.py
+++ cafe/application_service.py
@@ -1,7 +1,8 @@
 from cafe.models import Cafe, Order
 
 
-class OrderCoffeeUseCase:
+class OrderCoffeeService:
+    # same as usecase
     def order_coffee(self, cafe_id: int, customer_id: int):
         cafe = Cafe.objects.get(pk=cafe_id)
         order = Order.objects.create(cafe_id=cafe.id, customer_id=customer_id)
