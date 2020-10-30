146e58027516fd96bd1f2936aac47662c4ce9f28
suhyunbaik <suhyunbaik89@gmail.com>
2020-10-30 14:53:25 +0900

    :sparkles: define interface in repository pattern and implement cafe repository

--- cafe/object_service.py
+++ cafe/object_service.py
@@ -1,3 +1,23 @@
+import abc
+from typing import NoReturn
+
+from cafe.models import Cafe
 
 
-class CafeRepo()
+class Repository(abc.ABC):
+    # cafe interface
+    @abc.abstractmethod
+    def get(self, cafe_id: int):
+        pass
+
+    @abc.abstractmethod
+    def save(self, cafe: Cafe):
+        pass
+
+
+class CafeRepository(Repository):
+    def get(self, cafe_id: int) -> Cafe:
+        return Cafe.objects.get(pk=cafe_id)
+
+    def save(self, cafe: Cafe) -> NoReturn:
+        cafe.save()
