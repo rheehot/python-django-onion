e4a551ebe295e661b69321f4300f0f118574e3e4
suhyunbaik <suhyunbaik89@gmail.com>
2020-10-30 14:32:11 +0900

    :sparkles: add usecase

---
+++ cafe/usecase.py
@@ -0,0 +1,8 @@
+from cafe.models import Cafe, Order
+
+
+class OrderCoffeeUseCase:
+    def order_coffee(self, cafe_id: int, customer_id: int):
+        cafe = Cafe.objects.get(pk=cafe_id)
+        order = Order.objects.create(cafe_id=cafe.id, customer_id=customer_id)
+        order.save()
