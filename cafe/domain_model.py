6e35eabfdfa16535fe52cb97f6207615a4b66777
suhyunbaik <suhyunbaik89@gmail.com>
2020-10-30 15:14:19 +0900

    :sparkles: add cafe and customer entity

---
+++ cafe/domain_model.py
@@ -0,0 +1,16 @@
+class Cafe:
+    def __init__(self, id: int, name: str):
+        self.id = id
+        self.name = name
+
+    def update(self):
+        pass
+
+    def save(self):
+        pass
+
+
+class Customer:
+    def __init__(self, id: int, name: str):
+        self.id = id
+        self.name = name
