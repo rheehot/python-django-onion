971d3ecd9745a2f04e5760452f3a60720b01ae29
suhyunbaik <suhyunbaik89@gmail.com>
2020-10-30 14:24:36 +0900

    :card_file_box: cafe, customer, order django schema

--- cafe/models.py
+++ cafe/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Cafe(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+
+
+class Customer(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+
+
+class Order(models.Model):
+    id = models.AutoField(primary_key=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    cafe = models.ForeignKey(Cafe, on_delete=models.CASCADE)
+
+
