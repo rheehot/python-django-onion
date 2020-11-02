78285e7767794748d9ee3d615717c450ac4820bb
suhyunbaik <suhyunbaik89@gmail.com>
2020-11-03 01:14:45 +0900

    :recycle: rename repository to object_service

--- cafe/application_service.py
+++ cafe/application_service.py
@@ -1,14 +1,13 @@
 import inject
 
 from cafe.models import Order
-from cafe.object_service import CafeRepository
+from cafe.object_service import CafeObjectService
 
 
 class OrderCoffeeService:
-    # same as usecase
     @inject.autoparams('cafe_repository')
-    def __init__(self, cafe_repository: CafeRepository):
-        self._cafe_repository = cafe_repository
+    def __init__(self, cafe_object_service: CafeObjectService):
+        self._cafe_object_service = cafe_object_service
 
     def order_coffee(self, cafe_id: int, customer_id: int):
         order = Order.objects.create(cafe_id=cafe_id, customer_id=customer_id)
